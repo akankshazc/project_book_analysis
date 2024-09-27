a632f4fbc45496a7a7657f087f7989fec0975681
Akanksha <akankshazc23@icloud.com>
2024-09-27 10:28:02 +0530

    Additional metadata script to extract from lemmatised files and corresponding metadata csv file.

--- Text_Preprocessing/extract_metadata.py
+++ Text_Preprocessing/extract_metadata.py
@@ -1,6 +1,8 @@
 import os
 import csv
 
+# Extract metadata before Preprocessing
+
 
 def extract_metadata(input_dirs):
 
@@ -30,7 +32,7 @@ def extract_metadata(input_dirs):
     # Convert to csv format and output
     keys = metadata[0].keys()
 
-    with open('metadata.csv', 'w', newline='') as output_file:
+    with open('Text_Preprocessing/metadata.csv', 'w', newline='') as output_file:
         dict_writer = csv.DictWriter(output_file, keys)
         dict_writer.writeheader()
         dict_writer.writerows(metadata)
@@ -41,3 +43,50 @@ def extract_metadata(input_dirs):
 
 # Example Use:
 # extract_metadata(['TMBD_Chapters', 'IR_Chapters'])
+
+
+# Extract Metadata after Preprocessing
+
+def extract_preprocessed_metadata(input_dirs):
+
+    # Extract series name from input_dir
+    series_initials = [input_dir.split(
+        '/')[1].split('_')[0] for input_dir in input_dirs]
+
+    metadata = []
+
+    for series, input_dir in zip(series_initials, input_dirs):
+
+        # Loop through all book files in the series
+        for filename in os.listdir(input_dir):
+
+            # Extract Metadata from filename (ignoring the lemma part of the file names)
+            book_number, book_initials, chapter_number, _ = filename.split('_')
+
+            # Original File Name
+            original_file_name = '_'.join(filename.split('_')[:3])
+
+            # Append each chapter with filename and metadata
+            metadata.append({'File Name': original_file_name+'.txt',
+                             'Lemmatized File Name': filename,
+                            'Series Code': series,
+                             'Book Number': book_number,
+                             'Book Code': book_initials,
+                             'Chapter Number': chapter_number
+                             })
+
+    # Convert to csv format and output
+    keys = metadata[0].keys()
+
+    with open('Text_Preprocessing/metadata_lemma.csv', 'w', newline='') as output_file:
+        dict_writer = csv.DictWriter(output_file, keys)
+        dict_writer.writeheader()
+        dict_writer.writerows(metadata)
+
+    print(
+        f'Metadata has been extract from lemmatized filenames for all files of {input_dirs} into metadata_lemma.csv')
+
+
+# Example Use:
+# extract_preprocessed_metadata(['Text_Preprocessing/TMBD_Chapters_lemma',
+#                               'Text_Preprocessing/IR_Chapters_lemma'])
